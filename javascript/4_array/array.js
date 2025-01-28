15e6623c5e3271a72829ae672703b38229fa8717
Priya4604 <dharashinipriya2827@gmail.com>
2025-01-28 17:09:09 +0530

    recent

--- javascript/4_array/array.js
+++ javascript/4_array/array.js
@@ -1,1 +1,60 @@
-g
+
+
+let arr = [10,"hello",true,30]
+
+console.log(arr);
+
+//! how to get the length of the array
+
+console.log(arr.length);
+
+//! Array Method
+
+//! 1. push()
+
+// it is used to add element at the end of the array
+
+arr.push("bye")
+console.log(arr);
+
+
+//! 2. pop()
+
+// it is used to remove element from the end.
+
+let names = ["deva","ruby","sandeep","rohit"]
+
+names.pop()
+console.log(names);
+
+
+//! 3. unshift()
+
+// it is used to add element at the starting of the array.
+
+
+names.unshift("jeeva")
+
+console.log(names);
+
+
+//! 4. shift()
+
+// it is used to remove an element from the starting of the array.
+
+let fruits = ["appe","mango","guva"]
+
+fruits.shift()
+console.log(fruits);
+
+//! 5. indexOf()
+
+// it is used to get the index of the given element
+
+let subject=["html","css","js","java"]
+
+console.log(subject.indexOf("js"));
+
+
+
+
